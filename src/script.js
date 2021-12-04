a81e97e5a264acd8cdacefb568dfb4be01473871
Eade Hemingway <eadehemingway@Eades-MacBook-Pro.local>
2021-12-04 17:45:06 +0000

    code showing nad canvas showing nice

--- src/script.js
+++ src/script.js
@@ -4,11 +4,24 @@ import vertShader from "./shaders/vert.vert";
 import "./style.css";
 
 const fragShaders = [oneFragShader, twoFragShader];
+const multiTileWrapper = document.querySelector(".multi-tile-wrapper");
 
 const REGL = require("regl");
-const canvases = document.querySelectorAll("canvas");
 
-canvases.forEach((canvas, i)=> {
+fragShaders.forEach((frag)=> {
+    const canvas = document.createElement("canvas");
+
+    const text = document.createElement("code");
+    text.textContent = frag;
+    const pre = document.createElement("pre");
+    pre.appendChild(text);
+
+    const wrapper = document.createElement("div");
+    wrapper.className = "frag-shader-wrapper";
+    wrapper.appendChild(pre);
+    wrapper.appendChild(canvas);
+
+    multiTileWrapper.appendChild(wrapper);
     const regl = REGL({ canvas });
     regl({
         attributes: {
@@ -16,7 +29,7 @@ canvases.forEach((canvas, i)=> {
         },
         count: 6,
         vert: vertShader,
-        frag: fragShaders[i],
+        frag: frag,
     })();
 });
 
