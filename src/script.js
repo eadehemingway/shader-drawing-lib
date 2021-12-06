7fc2043f686a60f3c2dacfb809899734c757a332
Eade Hemingway <eadehemingway@Eades-MacBook-Pro.local>
2021-12-06 12:59:40 +0000

    move into folders

--- src/script.js
+++ src/script.js
@@ -1,32 +1,32 @@
 import oneFragShader from "./shaders/1.frag";
-import twoFragShader from "./shaders/2.frag";
-import threeFragShader from "./shaders/3.frag";
-import fourFragShader from "./shaders/4.frag";
-import fiveFragShader from "./shaders/5.frag";
-import sixFragShader from "./shaders/6.frag";
-import sevenFragShader from "./shaders/7.frag";
-import eightFragShader from "./shaders/8.frag";
-import nineFragShader from "./shaders/9.frag";
-import tenFragShader from "./shaders/10.frag";
-import elevenFragShader from "./shaders/11.frag";
-import twelveFragShader from "./shaders/12.frag";
-import thirteenFragShader from "./shaders/13.frag";
-import fourteenthFragShader from "./shaders/14.frag";
-import fourteenAndHalfFragShader from "./shaders/14.5.frag";
-import fifteenthFragShader from "./shaders/15.frag";
-import sixteenthFragShader from "./shaders/16.frag";
-import seventeenthFragShader from "./shaders/17.frag";
-import eighteenth from "./shaders/18.frag";
-import nineteenth from "./shaders/19.frag";
-import twenty from "./shaders/20.frag";
-import twentyone from "./shaders/21.frag";
-import twentytwo from "./shaders/22.frag";
-import twentythree from "./shaders/23.frag";
-// import twentyfour from "./shaders/24.frag";
-// import twentyfive from "./shaders/25.frag";
-// import twentysix from "./shaders/26.frag";
-// import twentyseven from "./shaders/27.frag";
-// import twentyeight from "./shaders/28.frag";
+import twoFragShader from "./shaders/01.draw-lines/2.frag";
+import threeFragShader from "./shaders/01.draw-lines/3.frag";
+import fourFragShader from "./shaders/01.draw-lines/4.frag";
+import fiveFragShader from "./shaders/01.draw-lines/5.frag";
+import sixFragShader from "./shaders/01.draw-lines/6.frag";
+import sevenFragShader from "./shaders/01.draw-lines/7.frag";
+import eightFragShader from "./shaders/01.draw-lines/8.frag";
+import nineFragShader from "./shaders/01.draw-lines/9.frag";
+import tenFragShader from "./shaders/01.draw-lines/10.frag";
+import elevenFragShader from "./shaders/01.draw-lines/11.frag";
+import twelveFragShader from "./shaders/01.draw-lines/12.frag";
+import thirteenFragShader from "./shaders/02.modulus/13.frag";
+import fourteenthFragShader from "./shaders/02.modulus/14.frag";
+import fourteenAndHalfFragShader from "./shaders/02.modulus/14.5.frag";
+import fifteenthFragShader from "./shaders/02.modulus/15.frag";
+import sixteenthFragShader from "./shaders/02.modulus/16.frag";
+import seventeenthFragShader from "./shaders/02.modulus/17.frag";
+import eighteenth from "./shaders/02.modulus/18.frag";
+import nineteenth from "./shaders/02.modulus/19.frag";
+import twenty from "./shaders/02.modulus/20.frag";
+import twentyone from "./shaders/02.modulus/21.frag";
+import twentytwo from "./shaders/02.modulus/22.frag";
+import twentythree from "./shaders/02.modulus/23.frag";
+import twentyfour from "./shaders/03.abs/24.frag";
+import twentyfive from "./shaders/03.abs/25.frag";
+import twentysix from "./shaders/03.abs/26.frag";
+import twentyseven from "./shaders/03.abs/27.frag";
+import twentyeight from "./shaders/03.abs/28.frag";
 import vertShader from "./shaders/vert.vert";
 import { createHtml } from "./createHtml";
 import "./style.css";
@@ -48,13 +48,13 @@ const fragShaders = [
     fourteenthFragShader,
     fourteenAndHalfFragShader,
     fifteenthFragShader,
-    // sixteenthFragShader,
-    // seventeenthFragShader,
-    // eighteenth,
-    // nineteenth,
-    // twenty,
-    // twentyone,
-    // twentytwo,
+    sixteenthFragShader,
+    seventeenthFragShader,
+    eighteenth,
+    nineteenth,
+    twenty,
+    twentyone,
+    twentytwo,
     twentythree
 ];
 
