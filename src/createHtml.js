4e492b0ff5fae0c1d7ea2e04fa68bf5605cfe952
Eade Hemingway <eadehemingway@Eades-MacBook-Pro.local>
2021-12-10 11:00:35 +0000

    style tiles

--- src/createHtml.js
+++ src/createHtml.js
@@ -4,18 +4,21 @@ const multiTileWrapper = document.querySelector(".multi-tile-wrapper");
 function createHtml(frag, index){
 
     const canvas = document.createElement("canvas");
-
+    canvas.width = 500;
+    canvas.height = 500;
     const text = document.createElement("code");
     text.textContent = frag;
     const pre = document.createElement("pre");
     pre.appendChild(text);
 
     const wrapper = document.createElement("div");
+    const left = document.createElement("div");
     const title = document.createElement("h3");
     title.textContent = `Fragment #${index + 1}`;
-    wrapper.appendChild(title);
+    left.appendChild(title);
     wrapper.className = "frag-shader-wrapper";
-    wrapper.appendChild(pre);
+    left.appendChild(pre);
+    wrapper.appendChild(left);
     wrapper.appendChild(canvas);
 
     multiTileWrapper.appendChild(wrapper);
--- src/shaders/01.draw-lines/9.frag
+++ src/shaders/01.draw-lines/9.frag
@@ -6,11 +6,13 @@ void main(){
 
     float bottom_white_block = step(0.7, 1.0 - st.y);
     float top_white_block = step(0.35, st.y);
-    float horizontal_line = bottom_white_block + top_white_block;
+    float horizontal_line = bottom_white_block
+                            + top_white_block;
 
     float left_white_block = step(0.7, 1.0 - st.x);
     float right_white_block = step(0.35, st.x);
-    float vertical_line = left_white_block + right_white_block;
+    float vertical_line = left_white_block
+                        + right_white_block;
 
    float color = horizontal_line * vertical_line;
 
